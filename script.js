ddb3ea56b09b0ca5754ff436b7564279d07f533f
Anton Grozdev <anton.grozdev@opencode.com>
2021-04-08 21:50:49 +0300

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+
+
+
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+    //alert(y)
+
+    document.getElementById("result").innerHTML="newtext,<br><br>";
+    // res = document.getElementById("result");
+    // res.value = p;
     
 }
-        
+
+// var mySlider = document.getElementById("rate");
+// var sliderValue = document.getElementById("sliderValue");
+// sliderValue.innerHTML = mySlider.value;
+
+// mySlider.oninput = function() {
+//         sliderValue.innerHTML = mySlider.value;
+//         output.innerHTML = this.value;
+// };
+
+function updateRangeInput(val) {
+    document.getElementById('sliderValue').value=val; 
+  }
