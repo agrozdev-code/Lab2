9626f2c9b45574fcd68bdeb365f8dee84b303577
agrozdev-code <82119876+agrozdev-code@users.noreply.github.com>
2021-04-12 16:11:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
-
-
-
 function compute()
 {
-    p = document.getElementById("principal").value;
-    r = document.getElementById("rate").value;
-    y = document.getElementById("years").value;
-    //alert(y)
+    var p = document.getElementById("principal").value;
+    if (p == '' || p<=0) {
+        alert("Please, enter correct principal!");
+        document.getElementById("principal").focus();
+        return;
+    }
+    var r = document.getElementById("rate").value;
+    var y = document.getElementById("years").value;
+    var date = new Date().getFullYear();
 
-    document.getElementById("result").innerHTML="newtext,<br><br>";
-    // res = document.getElementById("result");
-    // res.value = p;
-    
+    document.getElementById("result").innerHTML='If you deposit <span id="yellowSpan">' + p + '</span>,<br>\at an intereset rate of <span id="yellowSpan">' + r + '</span>.<br>\
+    You will receive an amount of <span id="yellowSpan">' + (p*(r/100))*y + '</span>,<br>\
+    in the year <span id="yellowSpan">' + (parseInt(date)+parseInt(y)) + '</span>.<br>';
 }
-
-// var mySlider = document.getElementById("rate");
-// var sliderValue = document.getElementById("sliderValue");
-// sliderValue.innerHTML = mySlider.value;
-
-// mySlider.oninput = function() {
-//         sliderValue.innerHTML = mySlider.value;
-//         output.innerHTML = this.value;
-// };
-
-function updateRangeInput(val) {
-    document.getElementById('sliderValue').value=val; 
-  }
